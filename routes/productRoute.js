c3b196e35124aa03cfae8db065979b0c2142af73
Daniel-Paul-chukwudi <paulchukwudidaniel@gmail.com>
2025-10-01 08:03:28 -0700

    product route and controller

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,13 @@
-const {addProduct} = require('../controllers/productConroller')
+const {addProduct, getAProduct,getAllProduct, getCategories} = require('../controllers/productConroller')
 const express = require('express')
 const router = express()
+const uploads = require('../middleware/multer')
+
+router.post('/product',uploads.array('productImages',5),addProduct)
+// router.get('/product/:id',getAProduct)
+router.get('/product',getAllProduct)
+router.get('/product/categories',getCategories)
 
 
+module.exports = router
 
