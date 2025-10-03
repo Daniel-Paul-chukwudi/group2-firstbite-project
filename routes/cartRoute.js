0d8ea23d689565bdc6c9afe7f9bfdc84f348a28a
Daniel-Paul-chukwudi <paulchukwudidaniel@gmail.com>
2025-10-02 23:50:40 -0700

    yeahhh boiii

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const { addCart, getCart, updateCart, deleteAProduct, clearCart } = require('../controllers/cartController');
 
 router.post('/cart', addCart); 
-router.get('/Cart', getCart);
+router.get('/cart/:userId', getCart);
 router.put('/updateC', updateCart)
 router.delete('/cart', deleteAProduct)
 router.delete('/clear', clearCart)
