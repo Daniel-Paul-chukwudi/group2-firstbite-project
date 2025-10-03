59e048d7c475d794f09a58852308605889f27217
Daniel-Paul-chukwudi <paulchukwudidaniel@gmail.com>
2025-10-03 03:15:14 -0700

    yeahhh boiii

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const cartModel = require('../models/cartModel')
 const userModel = require('../models/userModel')
 
-const { createOrder, getAllOrders, getOneOrder, getOrdersByUser } = require('../controllers/orderController')
+const { createOrder, getAllOrders, getOneOrder, getOrdersByUser,reOrder,orderRating,orderSummary } = require('../controllers/orderController')
 
 const router = require('express').Router();
 
@@ -10,5 +10,8 @@ router.post('/create', createOrder);
 router.get('/', getAllOrders);
 router.get('/:id', getOneOrder);
 router.get('/user/:userId', getOrdersByUser);
+router.post('/reorder/:orderId', reOrder);
+router.put('/rate/:orderId', orderRating);
+router.get('/summary/:orderId', orderSummary);
 
 module.exports = router;
